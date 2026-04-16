79e289f2aa4907fa3c009e0ecf0bf4a1234fcfef
huzaifa <huzaifainshal@gmail.com>
2026-04-16 15:29:17 +0500

    `Feat` - auth page updated

--- src/features/auth/layout/AuthLayout.tsx
+++ src/features/auth/layout/AuthLayout.tsx
@@ -9,7 +9,18 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="w-full md:w-1/2">{children}</div>
       <div className={cn("hidden md:flex p-2 center w-1/2")}>
         <div className="relative overflow-hidden rounded-lg w-full h-full">
-          <Image src={BgImage} alt="" fill className="" />
+          <Image
+            src={BgImage}
+            alt=""
+            fill
+            className="object-top object-cover right-0"
+          />
+          <div className="absolute left-1/2 -translate-x-1/2 bottom-20 flex-col-2 center text-center">
+            <h1 className="text-white text-3xl font-bold whitespace-nowrap">
+              Every call, every task, every time.
+            </h1>
+            <p className="text-white text-lg font-medium">- Tenley</p>
+          </div>
         </div>
       </div>
     </div>
